e74350ed7a1e4dade88cc822b060d869cc6ebf66
Sanjeev Kumar <66abcxyz@gmail.com>
2017-11-24 22:54:35 +0530

    first commit

--- app/controllers/search_controller.rb
+++
@@ -1,7 +0,0 @@
-class SearchController < ApplicationController
-  before_action :authenticate_user!
-
-  def show
-    @users = User.search(params[:search])
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, :path => 'your-sent-items' do
+  resources :users, :path => 'user' do
     collection do
   		get 'search'
   	end
